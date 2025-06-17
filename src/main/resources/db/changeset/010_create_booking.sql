9edc41b3d762fd462eddfa1cc901dcce36a22aa9
Daria Glotova <8dranik8@gmail.com>
2025-06-17 09:19:56 +0300

    validation

--- src/main/resources/db/changeset/010_create_booking.sql
+++ src/main/resources/db/changeset/010_create_booking.sql
@@ -3,9 +3,9 @@ CREATE TABLE booking
     id           SERIAL PRIMARY KEY,
     user_id      INTEGER REFERENCES users (id) ON DELETE CASCADE,
     session_id   INTEGER REFERENCES session (id) ON DELETE CASCADE,
-    booking_time TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
-    status       booking_status DEFAULT 'ACTIVE' NOT NULL,
-    created_at   TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
-    updated_at   TIMESTAMP      DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(user_id, session_id)
+    booking_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status       booking_status     DEFAULT 'ACTIVE' NOT NULL,
+    created_at   TIMESTAMP          DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP          DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (user_id, session_id)
 );
