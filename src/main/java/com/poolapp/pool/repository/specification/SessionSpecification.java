b1f20f5384d671e8fcfe00055cfc943544526b99
Daria Glotova <8dranik8@gmail.com>
2025-07-04 09:47:07 +0300

    fixed specification

--- src/main/java/com/poolapp/pool/repository/specification/SessionSpecification.java
+++ src/main/java/com/poolapp/pool/repository/specification/SessionSpecification.java
@@ -10,7 +10,9 @@ public class SessionSpecification {
 
     public static Specification<Session> hasPoolName(String poolName) {
         return (root, query, cb) -> {
-            if (poolName == null || poolName.isBlank()) return null;
+            if (poolName == null || poolName.isBlank()) {
+                return null;
+            }
             Join<Object, Object> poolJoin = root.join("pool");
             return cb.equal(cb.lower(poolJoin.get("name")), poolName.toLowerCase());
         };
@@ -18,14 +20,18 @@ public class SessionSpecification {
 
     public static Specification<Session> hasStartTime(LocalDateTime startTime) {
         return (root, query, cb) -> {
-            if (startTime == null) return null;
+            if (startTime == null) {
+                return null;
+            }
             return cb.equal(root.get("startTime"), startTime);
         };
     }
 
     public static Specification<Session> hasEndTime(LocalDateTime endTime) {
         return (root, query, cb) -> {
-            if (endTime == null) return null;
+            if (endTime == null) {
+                return null;
+            }
             return cb.equal(root.get("endTime"), endTime);
         };
     }
